fb19c5427c6fd1a5ae9bf77e02cf2be3ec5a8458
Artem Hrechanychenko <a.hrechanychenko@temabit.com>
2024-03-30 11:55:06 +0200

    Update terraform

--- session3/providers.tf
+++ session3/providers.tf
@@ -1,13 +1,12 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
