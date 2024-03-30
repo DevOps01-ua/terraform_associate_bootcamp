769cd0eab677044acbffe2947f193c186169e0bd
Artem Hrechanychenko <a.hrechanychenko@temabit.com>
2024-03-30 11:40:01 +0200

    Update session3

---
+++ session3/main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "sample_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true # Internal domain name
+  enable_dns_hostnames = true # Internal host name
+
+  tags = {
+    Name = "sample-vpc"
+  }
+}
